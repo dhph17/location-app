3cf69bbab783545197f015377d2065dadc933226
Loc <npbl.cntt@gmail.com>
2024-04-14 22:39:45 +0700

    Flutter: fixed scroll page login

--- location_app/lib/src/resources/login.dart
+++ location_app/lib/src/resources/login.dart
@@ -1,196 +1,191 @@
-// import 'package:flutter/material.dart';
-
-// class LoginPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: const Color(0xff000D2D),
-//       body: ListView(
-//         children: [
-//           Expanded(
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.end,
-//               children: [
-//                 SizedBox(
-//                   height: MediaQuery.of(context).size.height * 0.01,
-//                 ),
-//                 SizedBox(
-//                   height: MediaQuery.of(context).size.height * 0.25,
-//                   width: MediaQuery.of(context).size.height * 0.6,
-//                   child: Image.asset('./assets/images/logo1.png'),
-//                 ),
-//                 SizedBox(
-//                   height: MediaQuery.of(context).size.height * 0.005,
-//                 ),
-//                 const Text(
-//                   'GPS Tracker',
-//                   style: TextStyle(
-//                     fontSize: 36,
-//                     color: Colors.white,
-//                     fontWeight: FontWeight.bold,
-//                     decoration: TextDecoration.underline,
-//                     decorationColor: Colors.white,
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//           Expanded(
-//             flex: 2,
-//             child: Card(
-//               shape: RoundedRectangleBorder(
-//                 borderRadius: BorderRadius.only(
-//                   topLeft: Radius.circular(30),
-//                   topRight: Radius.circular(30),
-//                 ),
-//               ),
-//               color: const Color(0xffDA9F5C),
-//               child: Padding(
-//                 padding:
-//                     const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-//                 child: Column(
-//                   crossAxisAlignment: CrossAxisAlignment.stretch,
-//                   children: [
-//                     const SizedBox(
-//                       height: 40,
-//                     ),
-//                     const Center(
-//                       child: Text(
-//                         'Welcome to GPS Tracker!!',
-//                         style: TextStyle(
-//                           fontSize: 24,
-//                           color: Colors.black,
-//                           fontWeight: FontWeight.w900,
-//                         ),
-//                       ),
-//                     ),
-//                     const SizedBox(
-//                       height: 20,
-//                     ),
-//                     const Center(
-//                       child: Text(
-//                         'Enter your email and password here',
-//                         style: TextStyle(
-//                           fontSize: 16,
-//                           color: Colors.black,
-//                           fontWeight: FontWeight.bold,
-//                         ),
-//                         textAlign: TextAlign.center,
-//                       ),
-//                     ),
-//                     const SizedBox(
-//                       height: 20,
-//                     ),
-//                     const Text(
-//                       'Email',
-//                       style: TextStyle(
-//                         fontSize: 16,
-//                         color: Colors.black,
-//                         fontWeight: FontWeight.w900,
-//                       ),
-//                     ),
-//                     const TextField(
-//                       decoration: InputDecoration(
-//                         hintText: 'Enter your email',
-//                         hintStyle: TextStyle(
-//                             // color: Colors.black,
-//                             ),
-//                       ),
-//                     ),
-//                     const SizedBox(
-//                       height: 20,
-//                     ),
-//                     const Text(
-//                       'Password',
-//                       style: TextStyle(
-//                         fontSize: 16,
-//                         color: Colors.black,
-//                         fontWeight: FontWeight.w900,
-//                       ),
-//                     ),
-//                     TextField(
-//                       obscureText: true, // Ẩn mật khẩu
-//                       decoration: InputDecoration(
-//                         labelStyle: const TextStyle(
-//                           color: Colors.black,
-//                           fontSize: 14,
-//                         ),
-//                         contentPadding:
-//                             const EdgeInsets.symmetric(vertical: 10),
-//                         border: OutlineInputBorder(
-//                           borderRadius: BorderRadius.circular(10),
-//                         ),
-//                         fillColor: const Color(0xffD6AC7D),
-//                         filled: true, //
-//                       ),
-//                     ),
-//                     const SizedBox(
-//                       height: 20,
-//                     ),
-//                     Center(
-//                       child: TextButton(
-//                         onPressed: () {},
-//                         child: const Text(
-//                           'Forgot password?',
-//                           style: TextStyle(
-//                             fontSize: 16,
-//                             color: Color(0xff390808),
-//                             height: 1.5,
-//                             fontWeight: FontWeight.bold,
-//                             decoration: TextDecoration.underline,
-//                             decorationColor: Color(0xff390808),
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                     Center(
-//                       child: ElevatedButton(
-//                         onPressed: () {},
-//                         style: ElevatedButton.styleFrom(
-//                           shape: RoundedRectangleBorder(
-//                             borderRadius: BorderRadius.circular(15),
-//                           ),
-//                           backgroundColor: const Color(0xff000D2D),
-//                           foregroundColor: const Color(0xffFFE8E8),
-//                         ),
-//                         child: const Padding(
-//                           padding:
-//                               EdgeInsets.symmetric(horizontal: 50, vertical: 5),
-//                           child: Text(
-//                             'Login',
-//                             style: TextStyle(
-//                               fontSize: 24,
-//                               fontWeight: FontWeight.bold,
-//                             ),
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xff000D2D),
-        body: ListView(
-          children: [],
-        ));
+      backgroundColor: const Color(0xff000D2D),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height *
+                0.35, // 40% của chiều cao màn hình
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.01,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  width: MediaQuery.of(context).size.height * 0.6,
+                  child: Image.asset('./assets/images/logo1.png'),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.005,
+                ),
+                const Text(
+                  'GPS Tracker',
+                  style: TextStyle(
+                    fontSize: 36,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                    decorationColor: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height *
+                0.65, // 60% của chiều cao màn hình
+            child: Card(
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              color: const Color(0xffDA9F5C),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Center(
+                      child: Text(
+                        'Welcome to GPS Tracker!!',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Center(
+                      child: Text(
+                        'Enter your email and password here',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Email',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelStyle: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 20),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        fillColor: const Color(0xffD6AC7D),
+                        filled: true,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Password',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    TextField(
+                      obscureText: true, // Ẩn mật khẩu
+                      decoration: InputDecoration(
+                        labelStyle: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 20),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        fillColor: const Color(0xffD6AC7D),
+                        filled: true, //
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Forgot password?',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xff390808),
+                            height: 1.5,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Color(0xff390808),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          backgroundColor: const Color(0xff000D2D),
+                          foregroundColor: const Color(0xffFFE8E8),
+                        ),
+                        child: const Padding(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
